e276ea2b56304ac48799afb16d73cca82f1d1776
Bruno Drugowick <bruno.drugowick@gmail.com>
2021-01-02 16:50:32 -0300

    Initial from start.spring.io

---
+++ src/main/java/dev/drugowick/hotwirespringbootthymeleafsample/HotwireSpringbootThymeleafSampleApplication.java
@@ -0,0 +1,13 @@
+package dev.drugowick.hotwirespringbootthymeleafsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotwireSpringbootThymeleafSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotwireSpringbootThymeleafSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/drugowick/hotwirespringbootthymeleafsample/HotwireSpringbootThymeleafSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.drugowick.hotwirespringbootthymeleafsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotwireSpringbootThymeleafSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
